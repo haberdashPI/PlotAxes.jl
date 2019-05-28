148cb04565123b1f31dc74be7f6c579110e88956
David Little <david.frank.little@gmail.com>
2019-05-28 14:24:19 -0400

    added old test back

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,35 +4,35 @@ using Dates
 using AxisArrays
 using Pkg
 
-# @testset "Can generate plotable data" begin
-#   data = AxisArray(rand(10,10,2,2),:a,:b,:c,:d)
-#   df, = PlotAxes.asplotable(data)
-#   @test size(df,1) == length(data)
-#   @test :a ∈ names(df)
-#   @test :b ∈ names(df)
-#   @test :c ∈ names(df)
-#   @test :d ∈ names(df)
-#   @test :value ∈ names(df)
+@testset "Can generate plotable data" begin
+  data = AxisArray(rand(10,10,2,2),:a,:b,:c,:d)
+  df, = PlotAxes.asplotable(data)
+  @test size(df,1) == length(data)
+  @test :a ∈ names(df)
+  @test :b ∈ names(df)
+  @test :c ∈ names(df)
+  @test :d ∈ names(df)
+  @test :value ∈ names(df)
 
-#   data = AxisArray(rand(10,10,2),:a,:b,:c)
-#   df, = PlotAxes.asplotable(data)
-#   @test size(df,1) == length(data)
+  data = AxisArray(rand(10,10,2),:a,:b,:c)
+  df, = PlotAxes.asplotable(data)
+  @test size(df,1) == length(data)
 
-#   data = AxisArray(rand(10,10),:a,:b)
-#   df, = PlotAxes.asplotable(data)
-#   @test size(df,1) == length(data)
+  data = AxisArray(rand(10,10),:a,:b)
+  df, = PlotAxes.asplotable(data)
+  @test size(df,1) == length(data)
 
-#   data = AxisArray(rand(10),:a)
-#   df, = PlotAxes.asplotable(data)
-#   @test size(df,1) == length(data)
+  data = AxisArray(rand(10),:a)
+  df, = PlotAxes.asplotable(data)
+  @test size(df,1) == length(data)
 
-#   data = AxisArray(rand(10),Axis{:time}(DateTime(1961,1,1):Day(1):DateTime(1961,1,10)))
-#   df, = PlotAxes.asplotable(data)
-#   @test size(df,1) == length(data)
+  data = AxisArray(rand(10),Axis{:time}(DateTime(1961,1,1):Day(1):DateTime(1961,1,10)))
+  df, = PlotAxes.asplotable(data)
+  @test size(df,1) == length(data)
 
-#   df, = PlotAxes.asplotable(rand(10,10),quantize=(5,5))
-#   @test size(df,1) == 25
-# end
+  df, = PlotAxes.asplotable(rand(10,10),quantize=(5,5))
+  @test size(df,1) == 25
+end
 
 ENV["R_HOME"]="*"
 # the latest Conda master simplifies the specification of conda channels
