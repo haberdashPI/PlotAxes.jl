13fd3309d886a2979d8a802b6cf331a3c1896a6d
David Little <david.frank.little@gmail.com>
2019-05-28 17:04:47 -0400

    removing unecessary `@static`

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,6 @@
 using Pkg
 
-@static if lowercase(get(ENV, "CI", "false")) == "true"
+if lowercase(get(ENV, "CI", "false")) == "true"
   ENV["R_HOME"]="*"
   # the latest Conda master simplifies the specification of conda channels
   pkg"add RCall Conda#fb9a112921656b9d38fbc92ef7dae540f1ba182b"
