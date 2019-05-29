ccf7355d7ba62045fc643e8aee106d07eb01925d
David Little <david.frank.little@gmail.com>
2019-05-29 12:16:54 -0400

    simplified build script

--- deps/build.jl
+++ deps/build.jl
@@ -3,8 +3,9 @@ using Pkg
 if lowercase(get(ENV, "CI", "false")) == "true"
   ENV["R_HOME"]="*"
   # the latest Conda master simplifies the specification of conda channels
-  pkg"add RCall Conda#fb9a112921656b9d38fbc92ef7dae540f1ba182b"
+  pkg"add Conda RCall "
   pkg"build RCall"
   using Conda
-  Conda.add("r-ggplot2",channel="r")
+  Conda.add_channel("r")
+  Conda.add("r-ggplot2")
 end
